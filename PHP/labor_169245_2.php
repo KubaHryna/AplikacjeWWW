790f32395bccf9e18286e945dc33f030aeb12479
KubaHryna <hrypa-jakub@wp.pl>
2024-10-24 16:24:23 +0200

    Lab_04

---
+++ PHP/labor_169245_2.php
@@ -0,0 +1,9 @@
+<?
+    $nr_indeksu='169245';
+    $nrGrupy='2';
+
+    echo 'Jakub Hryniewicz'.$nr_indeksu.'grupa'.$nrGrupy.'<br /><br />';
+
+    echo 'Zastosowanie metody include() <br />';
+
+?>
