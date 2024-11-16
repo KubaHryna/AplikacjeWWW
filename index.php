9518735ad06dfe62ac366aa04d722a775e23ca4b
KubaHryna <hrypa-jakub@wp.pl>
2024-11-16 19:06:54 +0100

    Lab_06

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+include("cfg.php");
+?>
+
+<!DOCTYPE html>
+<head>
+    <base href="/moj_projekt/">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel="icon" href="Zdjęcia/Icon.ico" sizes="32x32" type="image/x-icon">
+    <title>Filmy Oscarowe</title>
+    <script src="JS/timedate.js" type="text/javascript"></script>
+    <script src="JS/information.js" type="text/javascript"></script>
+</head>
+<body>
+    <div class="wrapper">
+        <?php
+           
+            if (isset($_GET['idp'])) {
+                
+                include("showpage.php");
+            } else {
+                
+                include('html/glowna.html');
+            }
+        ?>
+    </div>
+</body>
+</html>
